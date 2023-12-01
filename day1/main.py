133bc4804c7312adae32aab1cd0d98131e768d0a
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2023-12-02 00:15:58 +0100

    Changed code to python, and completed part 2 of day 1

---
+++ day1/main.py
@@ -0,0 +1,15 @@
+import sys
+
+D = open(sys.argv[1]).read().strip()
+ans = 0
+for line in D.split('\n'):
+    digits = []
+    for i, c in enumerate(line):
+        if c.isdigit():
+            digits.append(c)
+        for d, val in enumerate(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']):
+            if line[i:].startswith(val):
+                digits.append(str(d + 1))
+    score = int(digits[0] + digits[-1])
+    ans += score
+print(ans)
