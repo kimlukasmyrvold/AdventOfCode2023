6addb993fe1ebb8875243e461990e17d181e24d1
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2023-12-10 02:16:43 +0100

    Solution for day 9 part 1

---
+++ 9/main.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const fileContent = fs.readFileSync(process.argv[2], 'utf-8').trim().split(/\n/).map((line) => line.split(" ").map((a) => parseInt(a)));
+const results = [];
+fileContent.forEach(line => {
+    function getPattern(arr) {
+        const patternArray = [];
+        for (let i = 1; i < arr.length; i++) {
+            patternArray.push(arr[i] - arr[i - 1]);
+        }
+        return patternArray;
+    }
+
+    function nextValue(inputArray) {
+        let array = inputArray;
+        const newArrays = [];
+        while (!array.every(value => value === 0)) {
+            const newArray = getPattern(array);
+            newArrays.push(newArray)
+            array = newArray;
+        }
+        return inputArray[inputArray.length - 1] + addNext(newArrays);
+    }
+
+    function addNext(arr) {
+        const newArr = arr;
+        for (let i = arr.length - 1; i >= 0; i--) {
+            if (i !== arr.length - 1) newArr[i].push(newArr[i][newArr[i].length - 1] + newArr[i + 1][newArr[i + 1].length - 1])
+        }
+        return newArr[0].pop();
+    }
+
+    results.push(nextValue(line))
+});
+
+console.log(results.reduce((a, b) => a + b, 0))
