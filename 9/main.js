06d7c8cc34feff009bd84e23dd8390bfc20676de
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2023-12-10 02:37:05 +0100

    Updated code to ouput for part 1 and 2

--- 9/main.js
+++ 9/main.js
@@ -1,35 +1,47 @@
 const fs = require('fs');
 const fileContent = fs.readFileSync(process.argv[2], 'utf-8').trim().split(/\n/).map((line) => line.split(" ").map((a) => parseInt(a)));
-const results = [];
-fileContent.forEach(line => {
-    function getPattern(arr) {
-        const patternArray = [];
-        for (let i = 1; i < arr.length; i++) {
-            patternArray.push(arr[i] - arr[i - 1]);
+
+function solve(isPart2) {
+    const results = [];
+
+    fileContent.forEach(line => {
+        function nextValue(inputArray) {
+            let array = inputArray;
+            const newArrays = [];
+            while (!array.every(value => value === 0)) {
+                const newArray = getPattern(array);
+                newArrays.push(newArray)
+                array = newArray;
+            }
+
+            if (isPart2) return inputArray[0] - addNext(newArrays);
+            return inputArray[inputArray.length - 1] + addNext(newArrays);
         }
-        return patternArray;
-    }
 
-    function nextValue(inputArray) {
-        let array = inputArray;
-        const newArrays = [];
-        while (!array.every(value => value === 0)) {
-            const newArray = getPattern(array);
-            newArrays.push(newArray)
-            array = newArray;
+        function addNext(arr) {
+            const newArr = arr;
+            for (let i = arr.length - 1; i >= 0; i--) {
+                if (i === arr.length - 1) continue;
+
+                const b = newArr[i + 1][newArr[i + 1].length - 1];
+                if (isPart2) newArr[i].push(newArr[i][0] - b);
+                else newArr[i].push(newArr[i][newArr[i].length - 1] + b);
+            }
+            return newArr[0].pop();
         }
-        return inputArray[0] - addNext(newArrays);
+
+        results.push(nextValue(line))
+    });
+
+    return results.reduce((a, b) => a + b, 0);
+}
+
+function getPattern(arr) {
+    const patternArray = [];
+    for (let i = 1; i < arr.length; i++) {
+        patternArray.push(arr[i] - arr[i - 1]);
     }
+    return patternArray;
+}
 
-    function addNext(arr) {
-        const newArr = arr;
-        for (let i = arr.length - 1; i >= 0; i--) {
-            if (i !== arr.length - 1) newArr[i].push(newArr[i][0] - newArr[i + 1][newArr[i + 1].length - 1])
-        }
-        return newArr[0].pop();
-    }
-
-    results.push(nextValue(line))
-});
-
-console.log(results.reduce((a, b) => a + b, 0))
+console.log("Answer1: %s\nAnswer2: %s", solve(false), solve(true));
